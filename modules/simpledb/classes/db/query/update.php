a0a5cf4920cd453f3ac6ba0d498a0945ff75e4f8
crystal@crystal-desktop <ebence88@gmail.com>
2010-10-10 13:35:39 +0200

    implemented update query builder

--- modules/simpledb/classes/db/query/update.php
+++ modules/simpledb/classes/db/query/update.php
@@ -5,8 +5,22 @@ class DB_Query_Update {
 
     public $table;
 
+    public $values;
+
+    public $conditions;
+
     public function table($table) {
         $this->table = $table;
+        return $this;
+    }
+
+    public function values($values) {
+        $this->values = $values;
+        return $this;
+    }
+
+    public function where() {
+        $this->conditions []= DB::create_expr(func_get_args());
     }
 
 }
