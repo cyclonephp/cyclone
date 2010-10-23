f46e152e9036f0894655393ea1a803ef2b59d701
crystal@crystal-desktop <ebence88@gmail.com>
2010-10-23 23:39:28 +0200

    attach-detach

--- modules/config/classes/config.php
+++ modules/config/classes/config.php
@@ -4,6 +4,10 @@ class Config {
 
     protected static $inst;
 
+    public $readers;
+
+    public $writers;
+
     public static function inst() {
         if (null == self::$inst) {
             self::$inst = new Config;
@@ -17,6 +21,23 @@ class Config {
 
     public function get($key) {
         
+    }
+
+    public function attach_reader(Config_Reader $reader) {
+        $this->readers []= $reader;
+    }
+
+    public function attach_writer(Config_Writer $writer) {
+        $this->writers []= $writer;
+    }
+
+    public function detach_reader(Config_Reader $reader) {
+        $key = array_search($reader, $this->readers);
+        if (false !== $key) {
+            unset($this->readers[$key]);
+        } else {
+            throw new Config_Exception('this reader was not attached previously');
+        }
     }
 
 }
--- modules/config/tests/config/test.php
+++ modules/config/tests/config/test.php
@@ -3,5 +3,19 @@
 
 class Config_Test extends Kohana_Unittest_TestCase {
 
+    /**
+     * @expectedException Config_Exception
+     */
+    public function testAttachDetach() {
+        $file_reader = new Config_Reader_File;
+        $db_reader = new Config_Reader_Database;
+        Config::inst()->attach_reader($file_reader);
+        Config::inst()->attach_reader($db_reader);
+        $this->assertEquals(Config::inst()->readers, array($file_reader, $db_reader));
+        Config::inst()->detach_reader($file_reader);
+        Config::inst()->detach_reader($db_reader);
+        $this->assertEquals(Config::inst()->readers, array());
+        Config::inst()->detach_reader($db_reader);
+    }
     
 }
