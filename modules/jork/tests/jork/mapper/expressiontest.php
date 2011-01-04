f74a53fa432fe176e2f0bfe3bbad90199693b549
crystal@crystal-desktop <ebence88@gmail.com>
2011-01-04 19:37:00 +0100

    formatting

--- modules/jork/tests/jork/mapper/expressiontest.php
+++ modules/jork/tests/jork/mapper/expressiontest.php
@@ -7,7 +7,8 @@ class JORK_Mapper_ExpressionTest extends Kohana_Unittest_TestCase {
      */
     public function testExpression() {
         $jork_query = new JORK_Query_Select;
-        $jork_query->select(DB::expr('{user.id} || {user.name} || {user.email} || {user.posts.id}'))->from('Model_User user');
+        $jork_query->select(DB::expr('{user.id} || {user.name} || {user.email} || {user.posts.id}'))
+                ->from('Model_User user');
         $mapper = new JORK_Mapper_Select($jork_query);
         list($db_query, ) = $mapper->map();
         $jork_query->select(DB::expr('{user.posts}'));
