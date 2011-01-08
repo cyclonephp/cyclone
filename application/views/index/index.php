66ac45a17ef5614eb357b467b35312238665a3ca
crystal@crystal-desktop <ebence88@gmail.com>
2011-01-08 18:20:38 +0100

    adding some info to website main page (sorry, it's not a jork-related stuff

--- application/views/index/index.php
+++ application/views/index/index.php
@@ -4,3 +4,24 @@
 <ul>
 	<li><a href="guide">user guide</li>
 </ul>
+
+
+<h3>What is CyclonePHP?</h3>
+
+CyclonePHP is a featureful, enterprise-ready application development
+framework for PHP.
+
+<h3>Main features</h3>
+<ul>
+	<li>fast bootstrap mechanism</li>
+	<li>heavyweight ORM library</li>
+	<li>RAD features</li>
+	<li>originally built on Kohana</li>
+	<li>CSSMin, JSMin integration</li>
+	<li>Easy integration with other libraries</li>
+	<li>Loosely coupled, CyclonePHP libraries can be used as standalone libraries</li>
+</ul>
+
+<h3>When to use CyclonePHP?</h3>
+<p>CyclonePHP is designed for building big applications. If you have to deal with a
+complex model layer, or you have to create tons of forms quickly, or you have</p>
