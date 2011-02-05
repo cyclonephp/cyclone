35d208810ddfcf427788811a9aa63f9bd50e33ae
Lajos Pajger <pajla@cyclonephp.com>
2011-02-05 21:00:14 +0100

    implement FileSystem::autoloader_kohana

--- modules/core/classes/filesystem.php
+++ modules/core/classes/filesystem.php
@@ -2,7 +2,7 @@
 class FileSystem {
 
     public static function find_file($rel_filename){
-        if(file_exists(APPPATH.$rel_filename)){
+        if(is_file(APPPATH.$rel_filename)){
             return APPPATH.$rel_filename;
         }
 
@@ -10,7 +10,7 @@ class FileSystem {
         if($dir_handle){
             while(FALSE !== ($fname = readdir($dir_handle))){
                 if(($fname != '.') && ($fname != '..') ){
-                    if(file_exists(MODPATH.$fname.$rel_filename)){
+                    if(is_file(MODPATH.$fname.$rel_filename)){
                         closedir($dir_handle);
                         return MODPATH.$fname.$rel_filename;
                     }
@@ -19,20 +19,22 @@ class FileSystem {
             closedir($dir_handle);
         }
 
-        if(file_exists(SYSPATH.$filename)){
+        if(is_file(SYSPATH.$filename)){
             return SYSPATH.$filename;
         }
 
         // throw exception if couldn't find the file
     }
 
-    /**
-     * TODO pajla
-     */
     public static function autoloader_kohana($classname){
-        $rel_path = APPPATH.'classes'. DirectoryIterator::str_replace('_', '/', $classname).'php';
-        include $this->find_file($classname);
-
+       $classname = strtolower($classname);
+       $rel_filename = 'classes/'.str_replace('_', DIRECTORY_SEPARATOR, $classname).'php';
+       $result = FileSystem::find_file($rel_filename);
+       if($result){
+        include_once $result;
+        return TRUE;
+       }
+       return FALSE;
     }
 
     /**
--- modules/core/test/core/filesystemtest.php
+++ modules/core/test/core/filesystemtest.php
@@ -12,5 +12,19 @@ class Core_FileSystemTest extends Kohana_Unittest_TestCase {
         $test_abs_path = FileSystem::find_file('classes/file.php');
         $this->assertEquals($test_abs_path, SYSPATH.'classes/file.php');
     }
+
+    public function testAutloder_kohana(){
+        $test_classname = 'Filesystem';
+        $this->assertEquals(TRUE, FileSystem::autoloader_kohana($test_classname));
+        $this->assertTrue(class_exists($test_classname));
+
+        $test_classname = 'Controller_Core';
+        $this->assertEquals(TRUE, FileSystem::autoloader_kohana($test_classname));
+        $this->assertTrue(class_exists($test_classname));
+
+        $test_wrong_classname = 'Its_Coudnt_Exist';
+        $this->assertFalse(FileSystem::autoloader_kohana($test_wrong_classname));
+        $this->assertFalse(class_exists($test_wrong_classname));
+    }
 }
 ?>
