f6d7324aaede52b05b64f55a160704eb10b9a1e9
Bence Eros <crystal@cyclonephp.com>
2011-02-17 23:08:03 +0100

    added phpdoc @author and @package to config module classes

--- modules/config/classes/config.php
+++ modules/config/classes/config.php
@@ -1,5 +1,9 @@
 <?php
 
+/**
+ * @author Bence Eros <crystal@cyclonephp.com>
+ * @package Config
+ */
 class Config {
 
     const NOT_FOUND = -1;
--- modules/config/classes/config/reader/file.php
+++ modules/config/classes/config/reader/file.php
@@ -1,5 +1,9 @@
 <?php
 
+/**
+ * @author Bence Eros <crystal@cyclonephp.com>
+ * @package Config
+ */
 class Config_Reader_File implements Config_Reader {
 
     public $root_path;
