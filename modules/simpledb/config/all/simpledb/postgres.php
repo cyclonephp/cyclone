be2cb9eb48061a39508a8ac894522c4357fb9a3e
Bence Eros <crystal@cyclonephp.com>
2011-02-28 17:30:01 +0100

    refactored SimpleDB unittests

---
+++ modules/simpledb/config/all/simpledb/postgres.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+    'adapter' => 'postgres',
+    'connection' => array(
+        'username' => 'simpledb',
+        'password' => '',
+        'database' => 'simpledb',
+        'host' => 'localhost'
+    )
+);
