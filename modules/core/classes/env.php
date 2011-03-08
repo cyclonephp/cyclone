b152f5412b780ffb8188656c03ae5ecc27fefeb6
Bence Eros <crystal@cyclonephp.com>
2011-03-08 01:43:15 +0100

    added some docs to the Env class

--- modules/core/classes/env.php
+++ modules/core/classes/env.php
@@ -1,5 +1,9 @@
 <?php
-
+/**
+ * Static class that represents runtime environment configuration.
+ *
+ * @author Bence Eros <crystal@cyclonephp.com>
+ */
 class Env {
 
     const DEV = 'dev';
@@ -12,10 +16,26 @@ class Env {
 
     public static $is_windows;
 
+    /**
+     * Marks the current enviroment, it's value is recommended to be one of Env::DEV,
+     * Env::TEST or Env::PROD
+     *
+     * @var string
+     * @see Env::init()
+     */
     public static $current;
 
     public static $magic_quotes;
 
+    public static $charset = 'utf-8';
+
+    /**
+     * Sets up Env::$is_cli, Env::$is_windows and Env::$current. The last one
+     * is loaded from the CYCLONEPHP_ENV environment variable, or defaults to
+     * Env::DEV if not found.
+     *
+     * @usedby Env::init_legacy()
+     */
     public static function init() {
         self::$is_cli = PHP_SAPI === 'cli';
         self::$is_windows = DIRECTORY_SEPARATOR == '\\';
@@ -24,8 +44,16 @@ class Env {
             self::$current = self::DEV;
         }
         self::$magic_quotes = get_magic_quotes_gpc();
+
+        $_GET = Kohana::sanitize($_GET);
+        $_POST = Kohana::sanitize($_POST);
+        $_COOKIE = Kohana::sanitize($_COOKIE);
     }
 
+    /**
+     * Calls Env::init(), then populates the corresponding static variables in
+     * the Kohana class.
+     */
     public static function init_legacy() {
         self::init();
         Kohana::$environment = self::$current;
