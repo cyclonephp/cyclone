78d8b5ad34abdd445ce8b83c9a174b43edac1d1b
Bence Eros <crystal@cyclonephp.com>
2011-08-14 22:47:55 +0200

    merging config lib to system

--- system/classes/config.php
+++ system/classes/config.php
@@ -1,153 +1,107 @@
-<?php defined('SYSPATH') or die('No direct script access.');
+<?php
+
 /**
- * Wrapper for configuration arrays. Multiple configuration readers can be
- * attached to allow loading configuration from files, database, etc.
- *
- * @package    Kohana
- * @category   Configuration
- * @author     Kohana Team
- * @copyright  (c) 2009 Kohana Team
- * @license    http://kohanaphp.com/license
+ * @author Bence Eros <crystal@cyclonephp.com>
+ * @package Config
  */
 class Config {
 
-	// Singleton static instance
-	protected static $_instance;
+    const NOT_FOUND = -1;
 
-	/**
-	 * Get the singleton instance of Kohana_Config.
-	 *
-	 *     $config = Kohana_Config::instance();
-	 *
-	 * @return  Kohana_Config
-	 */
-	public static function instance()
-	{
-		if (self::$_instance === NULL)
-		{
-			// Create a new instance
-			self::$_instance = new self;
-		}
+    protected static $_instances = array();
 
-		return self::$_instance;
-	}
+    public $readers = array();
 
-	// Configuration readers
-	protected $_readers = array();
+    public $writers;
 
-	/**
-	 * Attach a configuration reader. By default, the reader will be added as
-	 * the first used reader. However, if the reader should be used only when
-	 * all other readers fail, use `FALSE` for the second parameter.
-	 *
-	 *     $config->attach($reader);        // Try first
-	 *     $config->attach($reader, FALSE); // Try last
-	 *
-	 * @param   object   Kohana_Config_Reader instance
-	 * @param   boolean  add the reader as the first used object
-	 * @return  $this
-	 */
-	public function attach(Kohana_Config_Reader $reader, $first = TRUE)
-	{
-		if ($first === TRUE)
-		{
-			// Place the log reader at the top of the stack
-			array_unshift($this->_readers, $reader);
-		}
-		else
-		{
-			// Place the reader at the bottom of the stack
-			$this->_readers[] = $reader;
-		}
+    /**
+     *
+     * @param string $name config setup name
+     * @return Config
+     */
+    public static function inst($name = 'default') {
+        if ( ! array_key_exists($name, self::$_instances)) {
+            self::$_instances[$name] = new Config;
+        }
+        return self::$_instances[$name];
+    }
 
-		return $this;
-	}
+    public static function setup($setupfile = 'setup') {
+        $reader = new Config_Reader_File;
+        foreach ($reader->read($setupfile) as $name => $instance) {
+            $cfg = new Config;
+            $cfg->readers = &$instance['readers'];
+            $cfg->writers = &$instance['writers'];
+            self::$_instances [$name]= $cfg;
+        }
+    }
 
-	/**
-	 * Detach a configuration reader.
-	 *
-	 *     $config->detach($reader);
-	 *
-	 * @param   object  Kohana_Config_Reader instance
-	 * @return  $this
-	 */
-	public function detach(Kohana_Config_Reader $reader)
-	{
-		if (($key = array_search($reader, $this->_readers)) !== FALSE)
-		{
-			// Remove the writer
-			unset($this->_readers[$key]);
-		}
+    private function __construct() {
+        // empty private constructor
+    }
 
-		return $this;
-	}
+    public function get($key) {
+        foreach ($this->readers as $reader) {
+            if (($tmp = $reader->read($key)) !== Config::NOT_FOUND) {
+                return $tmp;
+            }
+        }
+        throw new Config_Exception("no value found for key '$key'");
+    }
 
-	/**
-	 * Load a configuration group. Searches the readers in order until the
-	 * group is found. If the group does not exist, an empty configuration
-	 * array will be loaded using the first reader.
-	 *
-	 *     $array = $config->load($name);
-	 *
-	 * @param   string  configuration group name
-	 * @return  object  Kohana_Config_Reader
-	 * @throws  Kohana_Exception
-	 */
-	public function load($group)
-	{
-		foreach ($this->_readers as $reader)
-		{
-			if ($config = $reader->load($group))
-			{
-				// Found a reader for this configuration group
-				return $config;
-			}
-		}
+    public function set($key, $val) {
+        foreach ($this->writers as $writer) {
+            if ($writer->write($key, $val))
+                return;
+        }
+        throw new Config_Exception("none of the config writers were able to write value '$key'");
+    }
 
-		// Reset the iterator
-		reset($this->_readers);
+    public function prepend_reader(Config_Reader $reader) {
+        array_unshift($this->readers, $reader);
+    }
 
-		if ( ! is_object($config = current($this->_readers)))
-		{
-			throw new Kohana_Exception('No configuration readers attached');
-		}
+    public function prepend_writer(Config_Writer $writer) {
+        array_unshift($this->writers, $writer);
+    }
 
-		// Load the reader as an empty array
-		return $config->load($group, array());
-	}
+    public function prepend_mock() {
+        $this->prepend_reader(Config_Storage_Mock::inst());
+        $this->prepend_writer(Config_Storage_Mock::inst());
+    }
 
-	/**
-	 * Copy one configuration group to all of the other readers.
-	 * 
-	 *     $config->copy($name);
-	 *
-	 * @param   string   configuration group name
-	 * @return  $this
-	 */
-	public function copy($group)
-	{
-		// Load the configuration group
-		$config = $this->load($group);
+    public function attach_reader(Config_Reader $reader) {
+        $this->readers []= $reader;
+        return $this;
+    }
 
-		foreach ($this->_readers as $reader)
-		{
-			if ($config instanceof $reader)
-			{
-				// Do not copy the config to the same group
-				continue;
-			}
+    public function attach_writer(Config_Writer $writer) {
+        $this->writers []= $writer;
+        return $this;
+    }
 
-			// Load the configuration object
-			$object = $reader->load($group, array());
+    public function detach_reader(Config_Reader $reader) {
+        $key = array_search($reader, $this->readers);
+        if (false !== $key) {
+            unset($this->readers[$key]);
+            return $this;
+        } else {
+            throw new Config_Exception('this reader was not attached previously');
+        }
+    }
 
-			foreach ($config as $key => $value)
-			{
-				// Copy each value in the config
-				$object->offsetSet($key, $value);
-			}
-		}
+    public function detach_writer(Config_Writer $reader) {
+        $key = array_search($reader, $this->readers);
+        if (false !== $key) {
+            unset($this->writers[$key]);
+            return $this;
+        } else {
+            throw new Config_Exception('this reader was not attached previously');
+        }
+    }
 
-		return $this;
-	}
+    public static function load() {
 
-} // End Kohana_Config
+    }
+
+}
