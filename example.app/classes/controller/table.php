ae8e59fcfaed3d7e71deb8578de2830583a76efd
Bence Eros <crystal@cyclonephp.com>
2011-10-04 20:43:41 +0200

    refactoring VCS - this repo now only contains the core system library. No submodules etc

--- example.app/classes/controller/table.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-class Controller_Table extends Controller_App {
-
-    public function action_index() {
-        $this->params['table'] = new Table('sample');
-    }
-}
--- tools/debug/views/debug/arguments.php
+++
@@ -1,8 +0,0 @@
-<div>
-        <? foreach ($items as $name => $val) : ?>
-            <?= $name ?>
-        <span class="arg-val">
-            <pre><?= $val ?></pre>
-        </span>
-        <? endforeach; ?>
-</div>
