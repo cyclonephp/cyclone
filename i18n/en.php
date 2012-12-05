7dc8daa717ebfdb964546beffc6c4a9216446ead
Bence Eros <crystal@cyclonephp.org>
2012-12-05 23:42:57 +0100

    adding default error messages in english and hungarian

--- i18n/en.php
+++ i18n/en.php
@@ -1,3 +1,22 @@
-<?php 
+<?php
 
-return array();
+return array(
+    'error.not_empty' => 'Required field',
+    'error.min_length' => 'It must be at least :1 characters long',
+    'error.max_length' => 'It must be at most :1 characters long',
+    'error.exact_length' => 'It must be :1 characters long',
+    'error.email' => 'Invalid email address',
+    'error.email_domain' => 'Invalid email address',
+    'error.url' => 'Invalid URL',
+    'error.ip' => 'Invalid IP address',
+    'error.credit_card' => 'Invalid credit card number',
+    'error.phone' => 'Invalid phone number',
+    'error.alpha' => 'It should contain only letters',
+    'error.alpha_numeric' => 'It should contain only letters and numbers',
+    'error.alpha_dash' => 'Is should contain only letters, numbers, underscores and dashes',
+    'error.digit' => 'It should contain only digits',
+    'error.numeric' => 'Invalid number',
+    'error.range' => 'It must be a number between :1 and :2',
+    'error.decimal' => 'It must be a decimal number',
+    'error.color' => 'It must be a valid hexadecimal color code'
+);
--- i18n/hu.php
+++ i18n/hu.php
@@ -1,7 +1,22 @@
 <?php
 
 return array(
-    'error.not_empty' => 'kötelező kitölteni',
-    'error.max_length' => 'túl hosszú érték',
-    'error.min_length' => 'túl rövid érték'
+    'error.not_empty' => 'Kötelező kitölteni',
+    'error.min_length' => 'Legalább :1 karakter hosszúnak kell lennie',
+    'error.max_length' => 'Legfeljebb :1 karakter hosszú lehet',
+    'error.exact_length' => 'Pontosan :1 karakter hosszúnak kell lennie',
+    'error.email' => 'Érvénytelen email cím',
+    'error.email_domain' => 'Érvénytelen email cím',
+    'error.url' => 'Érvénytelen URL',
+    'error.ip' => 'Hibás IP cím',
+    'error.credit_card' => 'Érvénytelen kártyaszám',
+    'error.phone' => 'Érvénytelen telefonszám',
+    'error.alpha' => 'Csak betűket tartalmazhat',
+    'error.alpha_numeric' => 'Csak betűket és számokat tartalmazhat',
+    'error.alpha_dash' => 'Csak betűket, számokat, aláhúzást és kötőjelet tartalmazhat',
+    'error.digit' => 'Csak számjegyeket tartalmazhat',
+    'error.numeric' => 'Érvénytelen szám',
+    'error.range' => 'Egy :1 és :2 közötti számnak kell lennie',
+    'error.decimal' => 'Érvénytelen szám',
+    'error.color' => 'Hexadecimális színkódnak kell lennie'
 );
